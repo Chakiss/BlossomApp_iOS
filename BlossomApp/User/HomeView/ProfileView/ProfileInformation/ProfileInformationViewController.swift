efe9f8d6c2fd7ba93db48f25258e820afebdfd9d
CHAKRIT PANIAM <Chakrit69@gmail.com>
2021-07-11 12:39:14 +0700

    add file

--- BlossomApp/User/HomeView/ProfileView/ProfileInformation/ProfileInformationViewController.swift
+++ BlossomApp/User/HomeView/ProfileView/ProfileInformation/ProfileInformationViewController.swift
@@ -6,18 +6,47 @@
 //
 
 import UIKit
+import DLRadioButton
 
 class ProfileInformationViewController: UIViewController {
 
+    @IBOutlet weak var informationView: UIView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surNameTextField: UITextField!
+    @IBOutlet weak var birthDayTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBOutlet weak var gendorView: UIView!
+    @IBOutlet weak var manButton: DLRadioButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        informationView.addConerRadiusAndShadow()
+        
+        nameTextField.addBottomBorder()
+        surNameTextField.addBottomBorder()
+        birthDayTextField.addBottomBorder()
+        addressTextField.addBottomBorder()
     }
     
 
+    @objc @IBAction private func logSelectedButton(radioButton : DLRadioButton) {
+        
+        print(String(format: "%@ is selected.\n", radioButton.selected()!.titleLabel!.text!));
+        
+        
+    }
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -26,4 +55,15 @@ class ProfileInformationViewController: UIViewController {
     }
     */
 
+}
+
+
+extension UITextField {
+    func addBottomBorder(){
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width - 40, height: 1)
+        bottomLine.backgroundColor = UIColor.blossomLightGray.cgColor
+        borderStyle = .none
+        layer.addSublayer(bottomLine)
+    }
 }
